22a8a42707a7d50fbf42dc9f2b193f0f98041e16
MarF22 <xerif24gt@tutanota.com>
2022-11-22 12:01:36 +0000

    Add spheres to objects with imported data from file

--- Maquete Project/Assets/ReadData2.cs
+++ Maquete Project/Assets/ReadData2.cs
@@ -5,20 +5,53 @@ using System.IO;
 using System;
 using Newtonsoft.Json.Linq;
 using UnityEngine.UIElements;
+using System.Globalization;
+using Unity.VisualScripting;
 
 public class ReadData2 : MonoBehaviour
 {
     //public MapsService MapsService;
+    float[] x_DataArray, y_DataArray, vel_DataArray, dir_DataArray;
+
+    // Values modifiers
+    float offset_xData = 0f, offset_yData = 0f;
+    float scale_xData = 1f, scale_yData = 1f, scale_velData = 1f;
+
+    // Arrows
+    //      Height of Arrows Objects
+    float height_YArrows = 0f;
+    //      Dimention
+    float dim_Arrows = 2f;
+
+    
     int count = 0, countPlanes = 0, press = -1, fatorEscala = 20;
-    bool RuaAveiro = false;
-    float[] PollutionData = new float[3 * 63003 + 1];
-    float[] PollutionDataExtra = new float[3 * 63003 + 1];
-    GameObject[] PollutionGO_Spheres = new GameObject[2 * 63003];
-    GameObject[] PollutionGO_Planes = new GameObject[2 * 3 * 3 * 63003];
-    float[] PollutionData_Aveiro = new float[3 * 229443 + 1];
-    float[] PollutionDataExtra_Aveiro = new float[3 * 229443 + 1];
-    GameObject[] PollutionGO_Spheres_Aveiro = new GameObject[2 * 229443];
-    GameObject[] PollutionGO_Planes_Aveiro = new GameObject[2 * 3 * 3 * 229443];
+
+
+
+    
+    //bool RuaAveiro = false;
+
+    GameObject[] arrows_ObjectsArray;
+    //GameObject[] PollutionGO_Planes = new GameObject[2 * 3 * 3 * 63003];
+    //float[] PollutionData_Aveiro = new float[3 * 229443 + 1];
+    //float[] PollutionDataExtra_Aveiro = new float[3 * 229443 + 1];
+    //GameObject[] PollutionGO_Spheres_Aveiro = new GameObject[2 * 229443];
+    //GameObject[] PollutionGO_Planes_Aveiro = new GameObject[2 * 3 * 3 * 229443];
+    string fileToRead1 = "Assets/PollutionData/ConstituicaoB_NOx_9_test.dat";
+
+
+    int TotalLines(string filePath)
+    {
+        using (StreamReader r = new StreamReader(filePath))
+        {
+            int i = 0;
+            while (r.ReadLine() != null) 
+            { 
+                i++; 
+            }
+            return i;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -28,9 +61,22 @@ public class ReadData2 : MonoBehaviour
         string line, str;
         int k, l;
         float j;
-        float RaioEsfera = 3, RaioEsfera_Aveiro = 2;
+        //float RaioEsfera = 3, RaioEsfera_Aveiro = 2;
 
-        reader = new StreamReader("Assets/PollutionData/ConstituicaoB_NOx_9_test.dat");
+        // Count lines of the file
+        int fileLenght = TotalLines(fileToRead1);
+        
+        // Initializate Arrays with max lenght
+        x_DataArray = new float[fileLenght];
+        y_DataArray = new float[fileLenght];
+        vel_DataArray = new float[fileLenght];
+        dir_DataArray = new float[fileLenght];
+        arrows_ObjectsArray = new GameObject[fileLenght];
+
+
+        reader = new StreamReader(fileToRead1);
+
+        int countDataIndex = 0;
 
         while ((line = reader.ReadLine()) != null)
         {
@@ -40,13 +86,33 @@ public class ReadData2 : MonoBehaviour
             {
                 continue;
             }
-           
-            
-            string log = "x: " + words[0] + " y: " + words[1] + " vel: " + words[3] + " dir: " + words[4];
+
+            x_DataArray[countDataIndex] = offset_xData + scale_xData * float.Parse(words[0], CultureInfo.InvariantCulture.NumberFormat);
+
+            y_DataArray[countDataIndex] = offset_yData + scale_yData *  float.Parse(words[1], CultureInfo.InvariantCulture.NumberFormat);
+            vel_DataArray[countDataIndex] = scale_velData * float.Parse(words[3], CultureInfo.InvariantCulture.NumberFormat);
+            dir_DataArray[countDataIndex] = float.Parse(words[4], CultureInfo.InvariantCulture.NumberFormat);
+
+            //string log = "x: " + words[0] + " y: " + words[1] + " vel: " + words[3] + " dir: " + words[4];
+            string log = "y: " + y_DataArray[countDataIndex];
             Debug.Log(log);
+
+
+            arrows_ObjectsArray[countDataIndex] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             
+            arrows_ObjectsArray[countDataIndex].name = "Arrow_" + countDataIndex.ToString();
+            renderer = arrows_ObjectsArray[countDataIndex].GetComponent<Renderer>();
+
+            renderer.material.color = Color.green;
+            arrows_ObjectsArray[countDataIndex].transform.localScale = new Vector3(dim_Arrows, dim_Arrows, dim_Arrows);
+
+            arrows_ObjectsArray[countDataIndex].transform.position = new Vector3(x_DataArray[countDataIndex], height_YArrows , y_DataArray[countDataIndex]  );
+
             
 
+            arrows_ObjectsArray[countDataIndex].SetActive(true);
+
+            countDataIndex++;
 
         }
     }
