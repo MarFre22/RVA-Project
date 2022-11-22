a62b074869028245ec09af6784b885f812c61ea0
MarF22 <xerif24gt@tutanota.com>
2022-11-22 18:19:59 +0000

    Update ReadData2

--- Maquete Project/Assets/ReadData2.cs
+++ Maquete Project/Assets/ReadData2.cs
@@ -10,7 +10,7 @@ using Unity.VisualScripting;
 
 public class ReadData2 : MonoBehaviour
 {
-    //public MapsService MapsService;
+    // Arrays to save the imported file values
     float[] x_DataArray, y_DataArray, vel_DataArray, dir_DataArray;
 
     // Values modifiers
@@ -24,7 +24,7 @@ public class ReadData2 : MonoBehaviour
     float dim_Arrows = 2f;
 
     
-    int count = 0, countPlanes = 0, press = -1, fatorEscala = 20;
+    //int count = 0, countPlanes = 0, press = -1, fatorEscala = 20;
 
 
 
@@ -32,6 +32,7 @@ public class ReadData2 : MonoBehaviour
     //bool RuaAveiro = false;
 
     GameObject[] arrows_ObjectsArray;
+
     //GameObject[] PollutionGO_Planes = new GameObject[2 * 3 * 3 * 63003];
     //float[] PollutionData_Aveiro = new float[3 * 229443 + 1];
     //float[] PollutionDataExtra_Aveiro = new float[3 * 229443 + 1];
@@ -39,7 +40,7 @@ public class ReadData2 : MonoBehaviour
     //GameObject[] PollutionGO_Planes_Aveiro = new GameObject[2 * 3 * 3 * 229443];
     string fileToRead1 = "Assets/PollutionData/ConstituicaoB_NOx_9_test.dat";
 
-
+    // Function to count the max number of lines of the file
     int TotalLines(string filePath)
     {
         using (StreamReader r = new StreamReader(filePath))
@@ -78,6 +79,7 @@ public class ReadData2 : MonoBehaviour
 
         int countDataIndex = 0;
 
+        // Loop to analyse each line of the file
         while ((line = reader.ReadLine()) != null)
         {
             string[] words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
@@ -87,8 +89,8 @@ public class ReadData2 : MonoBehaviour
                 continue;
             }
 
+            // Save each parameter of the file to the arrays
             x_DataArray[countDataIndex] = offset_xData + scale_xData * float.Parse(words[0], CultureInfo.InvariantCulture.NumberFormat);
-
             y_DataArray[countDataIndex] = offset_yData + scale_yData *  float.Parse(words[1], CultureInfo.InvariantCulture.NumberFormat);
             vel_DataArray[countDataIndex] = scale_velData * float.Parse(words[3], CultureInfo.InvariantCulture.NumberFormat);
             dir_DataArray[countDataIndex] = float.Parse(words[4], CultureInfo.InvariantCulture.NumberFormat);
@@ -97,19 +99,16 @@ public class ReadData2 : MonoBehaviour
             string log = "y: " + y_DataArray[countDataIndex];
             Debug.Log(log);
 
-
+            // Create and edit arrows
             arrows_ObjectsArray[countDataIndex] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            
             arrows_ObjectsArray[countDataIndex].name = "Arrow_" + countDataIndex.ToString();
             renderer = arrows_ObjectsArray[countDataIndex].GetComponent<Renderer>();
-
             renderer.material.color = Color.green;
             arrows_ObjectsArray[countDataIndex].transform.localScale = new Vector3(dim_Arrows, dim_Arrows, dim_Arrows);
-
             arrows_ObjectsArray[countDataIndex].transform.position = new Vector3(x_DataArray[countDataIndex], height_YArrows , y_DataArray[countDataIndex]  );
 
             
-
+            //      Activate 
             arrows_ObjectsArray[countDataIndex].SetActive(true);
 
             countDataIndex++;
